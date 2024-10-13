3a5baad38c7384491158f9a5af3e6240ae95c2b2
chima-01 <divinechima76@gmail.com>
2024-10-13 15:45:04 -0700

    refcatored code to fix edge case

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -16,7 +16,7 @@ def canUnlockAll(boxes):
             if i in unlock_boxes:
                 if not isinstance(boxes[i], list):
                     return False
-                unique = list(set(boxes[i]) - unlock_boxes)
+                unique = list(unlock_boxes - set(boxes[i]))
                 for j in range(0, len(unique)):
                     if unique[j] < len(boxes):
                         unlock_boxes.update(boxes[unique[j]])
