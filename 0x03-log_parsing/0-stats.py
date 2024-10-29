d1907f3875455f058102e551f86e49f6ff4b4e3e
chima-01 <divinechima76@gmail.com>
2024-10-29 12:29:56 +0100

    adjusted module to meet task requirement'

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -92,7 +92,7 @@ def parse_line():
             line = line.split()
             count += 1
 
-            if line[-1]:
+            if line[-1] and len(line) > 6:
                 if line[-2] in status_codes:
                     data["total_size"] += int(line[-1])
                     update_status_code(data["status_codes"], line[-2])
