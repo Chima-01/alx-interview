94138393df1bf84d18a1ac1770fa96c22597efd8
chima-01 <divinechima76@gmail.com>
2024-12-02 19:31:49 +0100

    optimized code to make it faster an efficient

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,14 +14,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    fewest_coin_per_index = [float("inf")] * (total + 1)
-    fewest_coin_per_index[0] = 0
 
-    dp = fewest_coin_per_index
+    coins.sort(reverse=True)
+    total_cpy = total
+    count = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i - coin] + 1, dp[i])
+    for coin in coins:
+        while total_cpy >= coin:
+            total_cpy -= coin
+            count += 1
+        if total_cpy == 0:
+            break
 
-    return dp[total] if dp[total] != float("inf") else -1
+    return count if total_cpy == 0 else -1
