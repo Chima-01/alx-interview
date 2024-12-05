5bbecde7115be34fe29a3156bb30856bcc1a65e1
chima-01 <divinechima76@gmail.com>
2024-12-05 18:57:34 +0100

    refactored code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,7 +17,7 @@ def island_perimeter(grid):
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter += 40
+                perimeter += 4
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
                 if row > 0 and grid[row - 1][col] == 1:
