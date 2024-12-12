90130992de71c11751f3e4bf7a39dcec82f29fcf
chima-01 <divinechima76@gmail.com>
2024-12-12 11:04:04 +0100

    source code to solve primegame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    using prime numbers, game theory, and
+    algorithm optimization to solve a competitive game scenario.
+"""
+
+
+def isWinner(x, nums):
+    """
+        Args:
+            x: number of game rounds
+            num: An array of number to find prime within
+    """
+    if x <= 0 or not nums:
+        return None
+
+    players = {
+            'Maria': 0,
+            'Ben': 0
+            }
+
+    for i in range(x):
+        if nums[i] <= 1:
+            players["Ben"] += 1
+            continue
+
+        index = [True for num in range(nums[i] + 1)]
+
+        p = 2
+        while (p * p <= nums[i]):
+            if index[p] is True:
+                for j in range(p * p, nums[i] + 1, p):
+                    index[j] = False
+
+            p += 1
+
+        true_count = index.count(True)
+
+        if true_count % 2:
+            players['Ben'] += 1
+        else:
+            players['Maria']
+
+    if players['Maria'] == players['Ben']:
+        return None
+    else:
+        return 'Maria' if players['Maria'] > players['Ben'] else 'Ben'
